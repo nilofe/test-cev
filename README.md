a185e48d6dc8a09bcd88743c1aaf53361f54a454
nilofe <nyucragavilan@gmail.com>
2023-12-29 18:48:27 -0500

    update

--- README.md
+++
@@ -1,2 +0,0 @@
-# test-challege-IA
-challege IA test 
---
+++ app/pokemon_data.py
@@ -0,0 +1,6 @@
+pokemon_data = {
+    "bulbasaur": {"name": "Bulbasaur", "type": "Grass/Poison"},
+    "charmander": {"name": "Charmander", "type": "Fire"},
+    "squirtle": {"name": "Squirtle", "type": "Water"}
+}
+
