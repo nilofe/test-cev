b077c5ce10a7701bd2ebf3234b4a69e318da6507
nilofe <nyucragavilan@gmail.com>
2024-01-03 23:01:19 -0500

    update 1.5.

--- cp.py
+++ cp.py
@@ -25,7 +25,7 @@ def crear_instancia():
         "--provisioning-model=STANDARD",
         "--service-account=356036604186-compute@developer.gserviceaccount.com",
         "--scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/trace.append",
-        "--create-disk=auto-delete=yes,boot=yes,device-name=instance-1,image=projects/ubuntu-os-cloud/global/images/ubuntu-minimal-2210-kinetic-amd64-v20230602,mode=rw,size=10,type=projects/test-dev-54830/zones/us-central1-a/diskTypes/pd-balanced",
+        "--create-disk=auto-delete=yes,boot=yes,device-name=instance-1,image=projects/ubuntu-os-cloud/global/images/ubuntu-2004-focal-v20231213,mode=rw,size=10,type=projects/test-dev-54830/zones/us-central1-a/diskTypes/pd-balanced",
         "--no-shielded-secure-boot",
         "--shielded-vtpm",
         "--shielded-integrity-monitoring",
